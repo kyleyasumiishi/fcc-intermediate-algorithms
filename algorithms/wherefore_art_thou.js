9a37c006be8ed4b680fed7d6f5f2d5ce0b6f8848
Kyle <k.yasumiishi@gmail.com>
2018-06-18 10:57:21 -0700

    Passed wherefore_art_thou

---
+++ algorithms/wherefore_art_thou.js
@@ -0,0 +1,25 @@
+/*
+ * Looks through an array of objects (first argument) and returns an array of all objects that have matching name/value pairs (second argument).
+ * @param {Array} collection - Array of objects.
+ * @param {Object} source - Object containing name/value pairs.
+ * @return {Array} arr - Array of objects from collection that contains each name/value pair from source.
+ */
+function whatIsInAName(collection, source) {
+    // What's in a name?
+    var arr = [];
+    // Only change code below this line
+    for (let i = 0; i < collection.length; i++) {
+        let object = collection[i];
+        let isMatch = true;
+        Object.entries(source).forEach(function([key, value]) {
+            if (!(object.hasOwnProperty(key) && object[key] === value)) {
+                isMatch = false;
+            }
+        });
+        if (isMatch) {
+            arr.push(object);
+        }
+    }
+    // Only change code above this line
+    return arr;
+}
